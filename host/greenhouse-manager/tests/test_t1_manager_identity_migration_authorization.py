04442b9d3ae23f32ce06056c334b406a520f110e
Renguo_CHEN <ch.renguo@gmail.com>
2026-07-14 23:13:49 +0800

    test(m2): use canonical auth environment keys

--- host/greenhouse-manager/tests/test_t1_manager_identity_migration_authorization.py
+++ host/greenhouse-manager/tests/test_t1_manager_identity_migration_authorization.py
@@ -61,9 +61,9 @@ def _runtime(
         "mqtt_password_present": False,
         "mqtt_password_file_present": False,
         "mqtt_authentication_environment_baseline": {
-            "username": {"present": False, "nonempty": False},
-            "password": {"present": False, "nonempty": False},
-            "password_file": {"present": False, "nonempty": False},
+            "GH_MQTT_USERNAME": {"present": False, "nonempty": False},
+            "GH_MQTT_PASSWORD": {"present": False, "nonempty": False},
+            "GH_MQTT_PASSWORD_FILE": {"present": False, "nonempty": False},
         },
         "manager_runtime_uid": 999,
         "manager_runtime_gid": 999,
