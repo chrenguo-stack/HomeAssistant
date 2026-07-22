e83635f448cb112bf53e937bb32ed6e35ec169ac
Renguo_CHEN <ch.renguo@gmail.com>
2026-07-22 11:40:35 +0800

    feat(n2): add Stage2D8 G2 probe header

---
+++ firmware/esphome_rc/components/greenhouse_profile_isolated_device_g2_probe/stage2d8_g2_read_only_probe.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <string>
+
+#include "esphome/core/component.h"
+#include "../greenhouse_profile_isolated_acceptance/isolated_acceptance_package.h"
+#include "../greenhouse_profile_isolated_device_driver/isolated_device_driver.h"
+#include "../greenhouse_profile_isolated_device_driver/isolated_device_esp32_ports.h"
+
+namespace esphome::greenhouse_pairing_client {
+
+class Stage2D8SerialEvidenceSink final : public IsolatedAcceptanceEvidenceSink {
+ public:
+  bool write_redacted_json(const std::string &json) override;
+};
+
+class Stage2D8G2ReadOnlyProbe final : public Component {
+ public:
+  void set_partition_label(const std::string &value) {
+    this->partition_label_ = value;
+  }
+  void set_namespace_name(const std::string &value) {
+    this->namespace_name_ = value;
+  }
+  void set_build_binding(const std::string &value) {
+    this->build_binding_ = value;
+  }
+
+  void setup() override;
+  void dump_config() override;
+  float get_setup_priority() const override;
+
+ protected:
+  void emit_snapshot_() const;
+  void fail_closed_(const char *reason);
+
+  std::string partition_label_{};
+  std::string namespace_name_{};
+  std::string build_binding_{};
+
+  VolatileTestPersistenceKeyProvider test_key_provider_{};
+  EspIdfIsolatedPersistencePort persistence_{};
+  EspIdfIsolatedMqttPort mqtt_{};
+  IsolatedDeviceDriver driver_{};
+  Stage2D8SerialEvidenceSink evidence_sink_{};
+  IsolatedAcceptancePackage package_{};
+
+  bool configured_{false};
+  bool inspection_attempted_{false};
+  bool inspection_passed_{false};
+};
+
+}  // namespace esphome::greenhouse_pairing_client
