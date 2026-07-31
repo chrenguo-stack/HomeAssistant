ab412abb67189f6551335927a688ceb13e37a89a
Renguo_CHEN <ch.renguo@gmail.com>
2026-07-31 22:59:54 +0800

    test(n2): add G16 forensic repair shell entrypoint

---
+++ tests/h3_n2_stage2d9r_tls_candidate/test_h3_n2_stage2d9r_g3r_d2_17_g15_consumed_marker_state_g16_forensic_repair_20260731_v1.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+set -eu
+PYTHONDONTWRITEBYTECODE=1 python3 -B tests/h3_n2_stage2d9r_tls_candidate/test_h3_n2_stage2d9r_g3r_d2_17_g15_consumed_marker_state_g16_forensic_repair_20260731_v1.py
