87620ba217f68dbc45d5a4a0593300075a819710
Renguo_CHEN <ch.renguo@gmail.com>
2026-07-31 15:47:35 +0800

    test(n2): add G11 acceptance shell entrypoint

---
+++ tests/h3_n2_stage2d9r_tls_candidate/test_h3_n2_stage2d9r_g3r_d2_17_g11_target_mac_static_check_acceptance_20260731_v1.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -eu
+ROOT="$(CDPATH= cd -- "$(dirname -- "$0")/../.." && pwd)"
+PYTHONDONTWRITEBYTECODE=1 python3 -B "$ROOT/tests/h3_n2_stage2d9r_tls_candidate/test_h3_n2_stage2d9r_g3r_d2_17_g11_target_mac_static_check_acceptance_20260731_v1.py"
