b80fbb3907d364c58e8a8c45a16ac8881188e6fd
Renguo_CHEN <ch.renguo@gmail.com>
2026-07-31 16:11:40 +0800

    test(n2): add G11 physical decision shell gate

---
+++ tests/h3_n2_stage2d9r_tls_candidate/test_h3_n2_stage2d9r_g3r_d2_17_g11_physical_execution_decision_20260731_v1.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -eu
+export PYTHONDONTWRITEBYTECODE=1
+python3 -B tests/h3_n2_stage2d9r_tls_candidate/test_h3_n2_stage2d9r_g3r_d2_17_g11_physical_execution_decision_20260731_v1.py "$@"
