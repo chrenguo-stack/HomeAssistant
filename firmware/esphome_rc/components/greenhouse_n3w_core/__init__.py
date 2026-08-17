05c022d8cf0766e24ac16e22f794c03fc318108b
Renguo_CHEN <ch.renguo@gmail.com>
2026-08-17 09:16:15 +0800

    feat(n3w): expose simplified Phase 4 product runtime gate

--- firmware/esphome_rc/components/greenhouse_n3w_core/__init__.py
+++ firmware/esphome_rc/components/greenhouse_n3w_core/__init__.py
@@ -6,8 +6,10 @@ from esphome.components.esp32 import include_builtin_idf_component
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ["esp32"]
+AUTO_LOAD = ["json"]
 
 CONF_PHASE4_SOURCE_HARNESS = "phase4_source_harness"
+CONF_PHASE4_PRODUCT_RUNTIME = "phase4_product_runtime"
 
 greenhouse_n3w_core_ns = cg.esphome_ns.namespace("greenhouse_n3w_core")
 GreenhouseN3wCore = greenhouse_n3w_core_ns.class_("GreenhouseN3wCore", cg.Component)
@@ -16,6 +18,7 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(GreenhouseN3wCore),
         cv.Optional(CONF_PHASE4_SOURCE_HARNESS, default=False): cv.boolean,
+        cv.Optional(CONF_PHASE4_PRODUCT_RUNTIME, default=False): cv.boolean,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -25,6 +28,8 @@ async def to_code(config: dict) -> None:
     include_builtin_idf_component("esp_event")
     include_builtin_idf_component("esp_netif")
     include_builtin_idf_component("esp_wifi")
+    include_builtin_idf_component("esp_http_client")
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     cg.add(var.set_phase4_source_harness_enabled(config[CONF_PHASE4_SOURCE_HARNESS]))
+    cg.add(var.set_phase4_product_runtime_enabled(config[CONF_PHASE4_PRODUCT_RUNTIME]))
